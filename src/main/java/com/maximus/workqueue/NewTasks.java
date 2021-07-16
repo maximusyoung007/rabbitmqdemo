81c2a3c30838b9da9e25082b531c0162477b147b
杨文俊 <1091261998@qq.com>
2021-07-16 16:47:33 +0800

    hello world

---
+++ src/main/java/com/maximus/workqueue/NewTasks.java
@@ -0,0 +1,13 @@
+package com.maximus.workqueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class NewTasks {
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+
+    }
+}
