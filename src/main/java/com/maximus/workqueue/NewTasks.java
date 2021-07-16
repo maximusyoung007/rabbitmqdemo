aee04f18be2085c24d963497f45d8217905af182
杨文俊 <1091261998@qq.com>
2021-07-16 17:29:20 +0800

    taskqueue1

--- src/main/java/com/maximus/workqueue/NewTasks.java
+++ src/main/java/com/maximus/workqueue/NewTasks.java
@@ -5,9 +5,20 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class NewTasks {
-    private final static String QUEUE_NAME = "hello";
+    private final static String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (
+                Connection connection = factory.newConnection();
+                Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, false, false, false, null);
+            String message = String.join(" ", args);
+            channel.basicPublish("", TASK_QUEUE_NAME, null, message.getBytes());
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+
 
     }
 }
